54596c50d4fc4aaadf30b9292fc258f93499bc6a
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-04-08 23:24:21 -0400

    media uploaded file to be stored on a s3 bucket, heroku not handling persistant storage for media uploaded users

--- alsace/settings/base.py
+++ alsace/settings/base.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'storages',
+
 ]
 
 MIDDLEWARE = [
@@ -145,6 +148,16 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+if os.getenv('AWS_STORAGE_BUCKET_NAME'):
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % os.getenv('AWS_STORAGE_BUCKET_NAME')
+
+    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Wagtail settings
