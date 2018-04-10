909dad7f129e65f61701db8de811084fa6f89a4b
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-04-09 23:04:42 -0400

    added main-navigation styles and animation + alsa-coeur font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 import 'foundation-sites/dist/css/foundation.min.css';
+
 import './css/main.css';
+import './css/header.css';
 
 window.$(document).foundation();
 
-console.log('Hello World !');
+/*
+* Main navigation animation
+* */
+$(document).ready(() => {
+    document.querySelector('.hamburger').addEventListener('click',() => {
+
+    let isOpen = document.querySelector('.hamburger').classList
+        .toggle('open');
+
+    console.log(isOpen);
+
+    if(isOpen){
+        document.querySelector('#main-navigation').classList
+            .toggle('open');
+
+        document.querySelector('body').classList
+            .toggle('no-scroll');
+
+        window.scroll(0,0);
+
+    }else{
+         document.querySelector('#main-navigation').classList
+            .toggle('closing');
+
+        setTimeout(() => {
+            document.querySelector('#main-navigation').classList
+                .toggle('open');
+
+            document.querySelector('#main-navigation').classList
+                .toggle('closing');
+
+            document.querySelector('body').classList
+                .toggle('no-scroll');
+        }, 400);
+    }
+});
+});
+
