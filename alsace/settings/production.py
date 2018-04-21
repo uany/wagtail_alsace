ad1912c8aef2f54c79a1b0ec7cb2d0a1c2ceefc1
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-04-21 18:15:42 -0400

    allowed hosts

--- alsace/settings/production.py
+++ alsace/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 
 DEBUG = False
 SECRET_KEY = os.getenv('SECRET_KEY')
-ALLOWED_HOSTS = ['127.0.0.1', 'www.alsace.nyc', 'wagtailalsace.herokuapp.com', 'alsace.nyc']
+ALLOWED_HOSTS = ['www.alsace.nyc', 'wagtailalsace.herokuapp.com', 'alsace.nyc', 'smtp.mailgun.org']
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SESSION_COOKIE_SECURE = True
