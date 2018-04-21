427c6ae29d845e3718cd0ce3ae44be4374cfb005
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-04-21 17:33:14 -0400

    wsgi to handle production

--- alsace/settings/production.py
+++ alsace/settings/production.py
@@ -1,8 +1,9 @@
 import os
+from .base import *
 
 DEBUG = False
 SECRET_KEY = os.getenv('SECRET_KEY')
-ALLOWED_HOSTS = ['www.alsace.nyc', 'wagtailalsace.herokuapp.com', 'alsace.nyc']
+ALLOWED_HOSTS = ['127.0.0.1', 'www.alsace.nyc', 'wagtailalsace.herokuapp.com', 'alsace.nyc']
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SESSION_COOKIE_SECURE = True
